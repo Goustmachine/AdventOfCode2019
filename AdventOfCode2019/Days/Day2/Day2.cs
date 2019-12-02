7009b297fe57ff8e0fb8395416763c79291b063a
Gustav Larsson <goustmachine@gmail.com>
2019-12-02 20:32:21 +0100

    Small cleanup

--- AdventOfCode2019/Days/Day2/Day2.cs
+++ AdventOfCode2019/Days/Day2/Day2.cs
@@ -26,11 +26,15 @@ namespace AdventOfCode2019.Days.Day2
 
         public static void Run2()
         {
+            Console.WriteLine(BruteForce());
+        }
+
+        private static int BruteForce()
+        {
             const int expectedOutput = 19690720;
 
             var fileReader = new FileReader();
             var programLength = fileReader.ReadAllLinesAsList("Day2").Count;
-            var done = false;
             for (var i = 0; i < programLength; i++)
             {
                 for (var j = 0; j < programLength; j++)
@@ -39,32 +43,21 @@ namespace AdventOfCode2019.Days.Day2
                     var intCodeReader = new IntCodeReader(program);
                     intCodeReader.Replace(1, i);
                     intCodeReader.Replace(2, j);
-                    
+
                     intCodeReader.Run();
 
                     var result = intCodeReader.GetValueAtIndex(0);
                     if (result == expectedOutput)
                     {
-                        Console.WriteLine("Value found: " + intCodeReader.GetCombinedNounAndVerb(0));
-                        done = true;
+                        return intCodeReader.GetCombinedNounAndVerb(0);
                     }
-                    else if (result > expectedOutput)
+                    if (result > expectedOutput)
                     {
-                        Console.WriteLine("Value not found");
-                        done = true;
+                        throw new Exception("Value not found");
                     }
-
-                    if (done)
-                    {
-                        break;
-                    }
-                }
-
-                if (done)
-                {
-                    break;
                 }
             }
+            throw new Exception("Value not found");
         }
     }
 }
